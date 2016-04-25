ddbca09939b45837f9988fc4e68a8f48601230ba
wuyuO <huahua_lanh@163.com>
2016-04-25 20:32:06 +0800

    一个vue的练习项目

---
+++ src/components/pagebar.vue
@@ -0,0 +1,104 @@
+<style lang="less">    
+    ul,li{
+        margin: 0px;
+        padding: 0px;
+    }
+    li{
+        list-style: none
+    }
+    .page-bar li:first-child>a {
+       margin-left: 0px
+    }
+    .page-bar a{
+        border: 1px solid #ddd;
+        text-decoration: none;
+        position: relative;
+        float: left;
+        padding: 6px 12px;
+        margin-left: -1px;
+        line-height: 1.42857143;
+        color: #337ab7;
+        cursor: pointer
+    }
+    .page-bar a:hover{
+        background-color: #eee;
+    }
+    .page-bar .active a{
+        color: #fff;
+        cursor: default;
+        background-color: #337ab7;
+        border-color: #337ab7;
+    }
+    .page-bar i{
+        font-style:normal;
+        color: #d44950;
+        margin: 0px 4px;
+        font-size: 12px;
+    }
+</style>
+<template>
+    <div class="page-bar">
+        <ul>
+        <li v-if="showFirst"><a v-on:click="cur--">上一页</a></li>
+        <li v-for="index in indexs"  v-bind:class="{ 'active': cur == index}">
+            <a v-on:click="btnClick(index)">{{ index }}</a>
+            </li>
+            <li v-if="showLast"><a v-on:click="cur++">下一页</a></li>
+            <li><a>共<i>{{all}}</i>页</a></li>
+        </ul>
+    </div>
+</template>
+<script>
+    export default {
+        el:"#bar",
+        data:{
+            all:20, //总页数
+            cur:1,  //当前页数
+        },
+        computed: {
+            indexs: function(){
+              var left = 1
+              var right = this.all
+              var ar = [] 
+              if(this.all>= 11){
+                if(this.cur > 5 && this.cur < this.all-4){
+                        left = this.cur - 5
+                        right = this.cur + 4
+                }else{
+                    if(this.cur<=5){
+                        left = 1
+                        right = 10
+                    }else{
+                        right = this.all
+                        left = this.all -9
+                    }
+                }
+             }
+            while (left <= right){
+                ar.push(left)
+                left ++
+            }   
+            return ar
+           },
+           showLast: function(){
+                if(this.cur == this.all){
+                    return false
+                }
+                return true
+           },
+           showFirst: function(){
+                if(this.cur == 1){
+                    return false
+                }
+               return true
+           }
+        },
+        methods: {
+            btnClick: function(data){//页码点击事件
+                if(data != this.cur){
+                    this.cur = data 
+                }
+            }
+        }
+    }
+</script>
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+var path = require("path");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: "./src/entry.js",
+    output: {
+        path: path.join(__dirname,"./dist"),
+        filename: "[name].js",
+        hash: true
+    },
+    module: {
+        loaders: [
+            { test: /\.vue$/, loader: 'vue' },
+            { test: /\.js$/, loader: 'babel', exclude: /node_modules/ },
+            { test: /\.css$/, loader: "style!css!autoprefixer" },
+            { test: /\.less$/, loader: 'style!css!autoprefixer?{browsers:["last 2 version", "Firefox 15"]}!less'}
+        ]
+    },
+    vue: {
+        loaders: {
+            css: 'style!css!autoprefixer!less'
+        }
+    },
+    babel: {
+        presets: ['es2015'],
+        plugins: ['transform-runtime']
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+          title: 'vue_webpack',
+        template: path.join(__dirname,'./index.html'),
+        })
+    ],
+    resolve: {
+        // require时省略的扩展名，如：require('module') 不需要module.js
+        extensions: ['', '.js', '.vue'],
+        // 别名
+        alias: {
+            filter: path.join(__dirname, './src/filters'),
+            components: path.join(__dirname, './src/components')
+        }
+    },
+  devtool: '#source-map'
+};
