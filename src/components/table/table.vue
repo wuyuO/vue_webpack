d1150db1b93725699de2b1bb0b9115e605b57225
wuyuO <huahua_lanh@163.com>
2016-06-20 17:30:11 +0800

    设置内容页

---
+++ src/components/table/table.vue
@@ -0,0 +1,44 @@
+<style media="screen">
+  .m-table{width: 100%; padding: 15px 60px 0 15px;}
+  .m-table .table_my{background-color: #fff;}
+  .m-table .tt{padding: 10px ;border-top: 3px solid #ddd;border-bottom: 1px solid #ddd;}
+  .m-table .table_cnt{padding: 15px;}
+</style>
+<template >
+  <div class="m-table">
+    <div class="table_my">
+      <div class="tt">
+        <div>表格组件样式</div>
+      </div>
+      <div class="table_cnt">
+        <table class="table table-bordered ">
+          <thead>
+            <th class="text-center">table title</th>
+            <th class="text-center">table title</th>
+            <th class="text-center">table title</th>
+            <th class="text-center">table title</th>
+            <th class="text-center">table title</th>
+            <th class="text-center">table title</th>
+            <th class="text-center">table title</th>
+          </thead>
+          <tbody>
+            <tr class="text-center">
+              <td>this is data</td>
+              <td>this is data</td>
+              <td>this is data</td>
+              <td>this is data</td>
+              <td>this is data</td>
+              <td>this is data</td>
+              <td>this is data</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+  export default{
+
+  }
+</script>
