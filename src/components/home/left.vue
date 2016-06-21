c88e2eca94ebebe73e04949fa56b6a6fcd5003f4
wuyuO <huahua_lanh@163.com>
2016-06-21 20:23:15 +0800

    调整

--- src/components/home/left.vue
+++ src/components/home/left.vue
@@ -1,24 +1,25 @@
 <style media="screen" lang="less" scoped>
   .m-nav_menu{background-color: #233646;width: 220px;position: fixed; top:50px;z-index: 10001;height: 100%}
+  .m-nav_menu a{text-decoration: none;width:100%;display: inline-block;color: #a7b1c2;}
   .m-nav_menu > li{color: #a7b1c2;font-size:12px;font-weight: 600}
   .m-nav_menu > li.active{border-left:3px solid #18bc9c;background-color: #293846;}
   .m-nav_menu > li.active .first{color: #fff;}
   .m-nav_menu > li .first{padding:10px 15px 10px 25px;}
   .m-nav_menu > li:not(:first-child):hover{border-left:3px solid #18bc9c;}
-  .m-nav_menu > li > .first:hover{color: #fff;background-color: #293846;}
+  .m-nav_menu > li a > .first:hover{color: #fff;background-color: #293846;}
   .m-nav_menu > li:first-child{padding:0;font-weight: 100;}
+  .m-nav_menu >li a:hover{background-color: #293846;}
   .m-user{background-color: #303641;padding:15px; color:#fff}
   .m-user .left{width: 70px;position: absolute;}
   .m-user .right{width: 100%;padding-left: 80px;}
   .m-user img{width: 65px; height:65px;background-color:#303641;border:4px solid #aaa;}
 
   /*--child menu --*/
-  .m-nav_menu .child_menu{padding: 0 10px 0 48px}
-  .m-nav_menu .child_menu >li{padding: 8px 0;}
-  li.active .child_menu >li:hover,li.active .child_menu >li.active{color: #fff;}
+  .m-nav_menu .child_menu >li a{padding: 8px 8px 8px 48px; }
+  li.active .child_menu >li a:hover,li.active .child_menu >li.active a{color: #fff;}
 </style>
 <template >
-  <ul class="nav m-nav_menu">
+  <ul class=" m-nav_menu">
     <li class="nav-header">
       <div class="m-user f-posrel">
         <div class="left">
@@ -36,14 +37,18 @@
     <!-- start for -->
     <template v-for="(key, item) in menu">
       <li :class="{'active':key == title}">
-        <div v-on:click="select_menu(key)" class="first">
-          <i class="fa {{item.icon}}  f-mgright15 " aria-hidden="true"></i>{{key}}
-          <span v-if="item.new" class="label label-success f-mgleft15">new</span>
-          <i v-if="item.hasOwnProperty('child_menu')" class="fa fa-chevron-down pull-right " :class="{'f-rotate90':!item.status}" aria-hidden="true"></i>
-        </div>
-        <ul v-if="key == title" class="nav child_menu">
+        <a v-link="{ path:item.url }">
+          <div v-on:click="select_menu(key)" class="first">
+            <i class="fa {{item.icon}}  f-mgright15 " aria-hidden="true"></i>{{key}}
+            <span v-if="item.new" class="label label-success f-mgleft15">new</span>
+            <i v-if="item.hasOwnProperty('child_menu')" class="fa fa-chevron-down pull-right " :class="{'f-rotate90':!item.status}" aria-hidden="true"></i>
+          </div>
+        </a>
+        <ul v-if="key == title" class="child_menu">
           <template v-for="(index, el) in item.child_menu">
-            <li v-on:click="child_active(index)" :class="{'active':index == child_index}">{{el.name}} <span v-if="el.new" class="label label-warning pull-right">new</span></li>
+            <li v-on:click="child_active(index)" :class="{'active':index == child_index}">
+              <a v-link="{ path:el.url }">{{el.name}} <span v-if="el.new" class="label label-warning pull-right">new</span></a>
+            </li>
           </template>
         </ul>
       </li>
--- src/components/home/right.vue
+++ src/components/home/right.vue
@@ -1,52 +1,71 @@
 <style media="screen" lang="less" scoped>
-  .m-right{border-radius: 2px;position: fixed;right:6px; top:14%; background-color:rgba(9, 9, 9,0.65); padding:10px 5px;color: #bdc5d2;}
-  .m-right li{position: relative}
+  .m-right{ list-style: none;border-radius: 2px;position: fixed;right:6px; top:11%; background-color:rgba(9, 9, 9,0.65); color: #bdc5d2;}
+  .m-right li a{position: relative}
+  .m-right li {padding: 10px 5px 0 5px;}
   .m-right li .msg-num{font-size: 11px;box-shadow: 0 2px 0 rgba(0, 0, 0, 0.2);color: #fff;background-color: #f22613;position: absolute;top: -2px;right: 2px;padding: 0 4px;border-radius: 4px;}
   .m-right li .msg-num-red{background-color: #f22613;}
   .m-right li .msg-num-blue{background-color: #5bc0de;}
   .m-right li .msg-num-yell{background-color: #f0ad4e;}
   .m-right li .msg-num-green{background-color: #5cb85c;}
-  .m-right li:hover{ color: #fff;}
+  .m-right li a:hover{ color: #fff;}
+  .m-right li a{color: #bdc5d2; text-decoration: none;display: inline-block; }
+  .m-right li p{margin: 0}
 </style>
 <template >
-  <ul class="nav m-right text-center">
+  <ul class=" m-right text-center">
     <li>
-      <span class="msg-num msg-num-red">13</span>
-      <i class="fa fa-comments fa-2x" aria-hidden="true"></i>
-      <p class="f-font11">待沟通</p>
+      <a v-link="{ path:'/login' }">
+        <span class="msg-num msg-num-red">13</span>
+        <i class="fa fa-comments fa-2x" aria-hidden="true"></i>
+        <p class="f-font11">待沟通</p>
+      </a>
     </li>
     <li>
-      <span class="msg-num msg-num-blue">10</span>
-      <i class="fa fa-flag fa-2x" aria-hidden="true"></i>
-      <p class="f-font11">待沟通</p>
+      <a v-link="{ path:'/login' }">
+        <span class="msg-num msg-num-blue">10</span>
+        <i class="fa fa-flag fa-2x" aria-hidden="true"></i>
+        <p class="f-font11">待沟通</p>
+      </a>
     </li>
     <li>
-      <span class="msg-num msg-num-yell">3</span>
-      <i class="fa fa-adjust fa-2x" aria-hidden="true"></i>
-      <p class="f-font11">待沟通</p>
+      <a v-link="{ path:'/login'}">
+        <span class="msg-num msg-num-yell">3</span>
+        <i class="fa fa-adjust fa-2x" aria-hidden="true"></i>
+        <p class="f-font11">待沟通</p>
+      </a>
     </li>
     <li>
-      <span class="msg-num msg-num-green">1</span>
-      <i class="fa fa-gear fa-2x" aria-hidden="true"></i>
-      <p class="f-font11">待沟通</p>
+      <a v-link="{ path:'/login' }">
+        <span class="msg-num msg-num-green">1</span>
+        <i class="fa fa-gear fa-2x" aria-hidden="true"></i>
+        <p class="f-font11">待沟通</p>
+      </a>
     </li>
     <li>
-      <i class="fa fa-comments fa-2x" aria-hidden="true"></i>
-      <p class="f-font11">待沟通</p>
+      <a v-link="{ path:'/login' }">
+        <i class="fa fa-comments fa-2x" aria-hidden="true"></i>
+        <p class="f-font11">待沟通</p>
+      </a>
     </li>
     <li>
-      <span class="msg-num msg-num-yell">3</span>
-      <i class="fa fa-flag fa-2x" aria-hidden="true"></i>
-      <p class="f-font11">待沟通</p>
+      <a v-link="{ path:'/login' }">
+        <span class="msg-num msg-num-yell">3</span>
+        <i class="fa fa-flag fa-2x" aria-hidden="true"></i>
+        <p class="f-font11">待沟通</p>
+      </a>
     </li>
     <li>
-      <i class="fa fa-adjust fa-2x" aria-hidden="true"></i>
-      <p class="f-font11">待沟通</p>
+      <a v-link="{ path:'/login' }">
+        <i class="fa fa-adjust fa-2x" aria-hidden="true"></i>
+        <p class="f-font11">待沟通</p>
+      </a>
     </li>
     <li>
-      <span class="msg-num msg-num-green">3</span>
-      <i class="fa fa-gear fa-2x" aria-hidden="true"></i>
-      <p class="f-font11">待沟通</p>
+      <a v-link="{ path:'/login' }">
+        <span class="msg-num msg-num-green">3</span>
+        <i class="fa fa-gear fa-2x" aria-hidden="true"></i>
+        <p class="f-font11">待沟通</p>
+      </a>
     </li>
   </ul>
 </template>
